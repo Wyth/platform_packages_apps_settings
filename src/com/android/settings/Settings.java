c4256730ee605d1ba07b68354ae195de5b0c854f
Sven Dawitz <sven@dawitz.de>
2012-12-17 01:29:48 +0100

    QuickSettings: Added ProfileTile (1/2)
    see frameworks commit for details
    
    Patchset 2:
      using blunden's png resource
      making ProfileSettings reachable from the outside
    
    Patchset 3:
      The profile tile should not be available if profiles are turned off
    
    Side rant: I really hate manifest cleanups. i would wish for our
    settings not beeing such a mess and comply with fragments properly.
    
    Change-Id: I3e7bcce4b5f54b9bed065780779c9946a73936be
    
    Conflicts:
    
    	src/com/android/settings/Settings.java

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -851,4 +851,5 @@ public class Settings extends PreferenceActivity
     public static class TextToSpeechSettingsActivity extends Settings { /* empty */ }
     public static class AndroidBeamSettingsActivity extends Settings { /* empty */ }
     public static class WifiDisplaySettingsActivity extends Settings { /* empty */ }
+    public static class ProfilesSettingsActivity extends Settings { /* empty */ }
 }
